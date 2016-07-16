329eb8eda2f93726930aaab153004febb33be366
Brian Clark <brian.clark@steadyserv.com>
2016-07-16 14:56:36 -0400

    entrypoint mods

--- 7.0/jdk7/entrypoint.sh
+++ 7.0/jdk7/entrypoint.sh
@@ -9,36 +9,37 @@ render_template_dir() {
   done
 }
 
-if [ -n "$CONSUL_HOST" ]; then
-  CONSUL_HTTP_ADDR=${CONSUL_HOST}:${CONSUL_PORT-8500}
-fi
-
-if [ -n "$CONSUL_HTTP_ADDR" ]; then
-  render_template_dir /entrypoint.d
-  render_template_dir $CATALINA_BASE/bin
-  render_template_dir $CATALINA_BASE/conf
-  render_template_dir $CATALINA_BASE/lib
-  chmod 600 $CATALINA_BASE/conf/jmxremote.access $CATALINA_BASE/conf/jmxremote.password
-  chown tomcat: $CATALINA_BASE/conf/jmxremote.access $CATALINA_BASE/conf/jmxremote.password
-else
-  >&2 echo "Skipping consul-template... CONSUL_HOST or CONSUL_HTTP_ADDR not provided"
-fi
-
-if [ -d "/entrypoint.d" ]; then
-  for f in /entrypoint.d/*; do
-    echo "Sourcing $f..."
-    source $f
-  done
-fi
-
 if [ "$1" = "catalina.sh" ]; then
-  cd $CATALINA_BASE
+  if [ -n "$CONSUL_HOST" ]; then
+    CONSUL_HTTP_ADDR=${CONSUL_HOST}:${CONSUL_PORT-8500}
+  fi
+
+  if [ -n "$CONSUL_HTTP_ADDR" ]; then
+    render_template_dir /entrypoint.d
+    render_template_dir $CATALINA_BASE/bin
+    render_template_dir $CATALINA_BASE/conf
+    render_template_dir $CATALINA_BASE/lib
+    chmod 600 $CATALINA_BASE/conf/jmxremote.access $CATALINA_BASE/conf/jmxremote.password
+    chown tomcat: $CATALINA_BASE/conf/jmxremote.access $CATALINA_BASE/conf/jmxremote.password
+  else
+    >&2 echo "Skipping consul-template... CONSUL_HOST or CONSUL_HTTP_ADDR not provided"
+  fi
+
+  if [ -d "/entrypoint.d" ]; then
+    for f in /entrypoint.d/*; do
+      echo "Sourcing $f..."
+      source $f
+    done
+  fi
+
+  cd $CATALINA_BASE || exit 1
   chown -R tomcat:tomcat temp work logs webapps
   # chown -R root:tomcat webapps
   # chmod g+s webapps
   find lib/ -type f -exec chmod 644 {} \;
   find conf/ -type f ! -name 'jmxremote.*' -exec chmod 644 {} \;
   chown tomcat:tomcat conf/Catalina
+
   exec gosu tomcat "$@"
 fi
 
